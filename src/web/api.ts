d00b4d2e756825ad71152efa3398cd7c36648fc0
Mikaela Gurney <mikaelagurney@gmail.com>
2022-08-16 23:10:39 -0700

    Add basic crud

--- src/web/api.ts
+++ src/web/api.ts
@@ -2,6 +2,22 @@ import axios from 'axios'
 
 const BASE_URL = 'http://localhost:8080'
 
+type LineItem = {
+    id: string,
+    description: string,
+    amount: number
+}
+
+type Invoice = {
+    id: string,
+    lineItems: LineItem[]
+}
+type Project = {
+    id: string,
+    title: string,
+    invoice: Invoice
+}
+
 /** The API for the app, for querying, creating and updating projects and invoices */
 class InvoicelyApi {
   /** Returns the ID and title of every existing project */
@@ -18,6 +34,19 @@ class InvoicelyApi {
   static async getProject(id:string) {
     try {
       const req = await axios.get(`${BASE_URL}/${id}`)
+      const { project } = req.data
+      return project
+    } catch (err) {
+      throw new Error('Unable to fetch project')
+    }
+  }
+  // do the project updating here?
+  static async updateProject(id: string, projectData: Project) {
+    console.log(id, projectData)
+    try {
+      const req = await axios.post(`${BASE_URL}/${id}`, 
+        {project: projectData}
+      )
       const { project } = req.data
       return project
     } catch (err) {
--- src/web/components/Invoice.tsx
+++ src/web/components/Invoice.tsx
@@ -1,7 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import InvoicelyApi from '../api'
-import { stringify } from "querystring";
 
 type LineItem = {
     id: string,
@@ -20,9 +19,12 @@ type Project = {
     invoice: Invoice
 }
 
+
 export default function Invoice() {
-    const [project, setProject] = useState<Project|null>(null)
-     const { id } = useParams();
+    const [project, setProject] = useState<Project | null>(null)
+    const [invoiceData, setInvoiceData] = useState<Invoice | null>(null)
+
+    const { id } = useParams();
 
   // Fetch all projects from the API
   useEffect(() => {
@@ -35,6 +37,49 @@ export default function Invoice() {
   }, [])
     
 
+    const addInvoiceItem = async () => {
+        const newItem = {
+            id: "123",
+            description: "Test",
+            amount: 1234
+        }
+  
+        // might not need to set new
+        const newProject = project;
+        newProject!.invoice.lineItems = [...newProject!.invoice.lineItems, newItem]
+        console.log(newProject)
+        const updatedProject = await InvoicelyApi.updateProject(id!, newProject!)
+        setProject(updatedProject)
+    }
+
+    // delete invoiceItem
+    const deleteInvoiceItem = async (id: string = "123") => {
+        project!.invoice.lineItems = project!.invoice.lineItems.filter(v =>v.id !== id)
+        const updatedProject = await InvoicelyApi.updateProject(id!, project!)
+        setProject(updatedProject)
+    }
+
+    // edit invoiceItem
+  const editInvoiceItem = async (id: string = "123") => {
+        const newItem = {
+            id: "123",
+            description: "Testing",
+            amount: 5000
+        }
+      
+        // better as a reduce
+        project!.invoice.lineItems = project!.invoice.lineItems.map(v => {
+            if (v.id === id) {
+               return newItem;
+            }
+            else return v
+        })
+            
+        const updatedProject = await InvoicelyApi.updateProject(id!, project!)
+            setProject(updatedProject)
+        }
+    
+    
 
     return (
         project ? (
@@ -42,9 +87,15 @@ export default function Invoice() {
             <h1>{project.title}</h1>
             <ul>
             {project.invoice.lineItems.map(v => 
-                <li key={v.id}><span>{v.description}</span> <span>{v.amount}</span></li>
+                <li key={v.id}>
+                    <span>{v.description}</span>
+                    <span>{v.amount}</span>
+                </li>
             )}
-            </ul>
+                </ul>
+                <button onClick={() => addInvoiceItem()}>Add</button>
+                <button onClick={() => deleteInvoiceItem()}>delete</button>
+                <button onClick={() => editInvoiceItem()}>edit</button>
         </div>
         ) : (
             <div>No Project Found</div>
